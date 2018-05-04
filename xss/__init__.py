c86e513d042b77eaca78e4479b220490494a7c3d
Jorge Araya Navarro <jorge@esavara.cr>
2018-05-03 18:51:36 -0600

    fix: Remove auto-generated file

--- xss/__init__.py
+++ xss/__init__.py
@@ -1,6 +1,6 @@
 from .xss import *
 
-__version__ = "2.1"
+__version__ = "2.1.1"
 __author__ = "David McClosky (dmcc@bigaterisk.com)"
 __credits__ = "Made with help from Drew Perttula (drewp@bigasterisk.com)"
 __doc__ = """Python interface to XScreenSaver information
@@ -21,7 +21,7 @@ package, but the extension in XFree86 instead.
 
 Example
 =======
->>> import xss 
+>>> import xss
 >>> info = xss.get_info()
 >>> print info.idle
 10
@@ -45,27 +45,31 @@ typedef struct {
 } XScreenSaverInfo;
 
 The window attribute will be useless.  For state and kind, you can test the
-values against their named values 
+values against their named values
 (i.e. "print info.state == xss.ScreenSaverOff")
 
 As of version 2.1, we now include IdleTracker and XSSTracker, which are
 higher level interfaces.  Both keep track of the current idle time and tell
 you whether the user became idle or unidle since the last poll, when the
 next poll should take place, and the current idle time in milliseconds.
-IdleTracker is based on some threshold for idle time, while XSSTracker 
+IdleTracker is based on some threshold for idle time, while XSSTracker
 announces that the user is idle when the screensaver activates.  An example
 poller can be found at the end of this file (xss/__init__.py)."""
+
 
 class IdleTracker:
     """Keeps track of idle times, screensaver state, and tells
     you when you to querying it for the next idle time.  All times
     are in milliseconds.  IdleTracker indicates a change in state when
     your idle time exceeds a certain threshold.  See also XSSTracker."""
-    def __init__(self, when_idle_wait=5000, when_disabled_wait=120000,
-        idle_threshold=60000):
+
+    def __init__(self,
+                 when_idle_wait=5000,
+                 when_disabled_wait=120000,
+                 idle_threshold=60000):
         """when_idle_wait is the interval at which you should poll when
         you are already idle.  when_disabled_wait is how often you should
-        poll if information is unavailable (default: 2 minutes).  
+        poll if information is unavailable (default: 2 minutes).
         idle_threshold is the number of seconds of idle time to constitute
         being idle."""
         self.when_idle_wait = when_idle_wait
@@ -74,10 +78,11 @@ class IdleTracker:
         # check_idle will report whether we are idle or not.  all subsequent
         # calls will only tell you if the screensaver state has changed
         self.last_state = None
+
     def check_idle(self):
         """Returns a tuple:
         (state_change, suggested_time_till_next_check, idle_time)
-        
+
         suggested_time_till_next_check and idle_time is in milliseconds.
         state_change is one of:
             None - No change in state
@@ -88,20 +93,20 @@ class IdleTracker:
         Note that "disabled" will be returned every time there is an error."""
         try:
             self.info = get_info()
-        except RuntimeError: # XSS can raise a RuntimeError if the
-                             # XSS extension cannot be found.
+        except RuntimeError:  # XSS can raise a RuntimeError if the
+            # XSS extension cannot be found.
             return ("disabled", self.when_disabled_wait, 0)
 
         idle = self.info.idle
 
-        if idle > self.idle_threshold: # if we meet the threshold for being
-                                       # idle, we are now idle
+        if idle > self.idle_threshold:  # if we meet the threshold for being
+            # idle, we are now idle
             current_state = 'idle'
             # we use the standard polling interval
             wait_time = self.when_idle_wait
-        else:                          # otherwise, we are not idle
+        else:  # otherwise, we are not idle
             current_state = 'unidle'
-            # wait time is however long it will take for us to go over the 
+            # wait time is however long it will take for us to go over the
             # idle threshold
             wait_time = self.idle_threshold - idle
 
@@ -113,17 +118,19 @@ class IdleTracker:
         self.last_state = current_state
         return (change, wait_time, self.info.idle)
 
+
 class XSSTracker:
     """Keeps track of idle times, screensaver state, and tells you
     when you to querying it for the next idle time.  All times are
     in milliseconds.  XSSTracker indicates a change in state when your
     screensaver activates.  See also IdleTracker."""
+
     def __init__(self, when_idle_wait=5000, when_disabled_wait=120000):
         """when_idle_wait is the interval at which you should poll when
         you are already idle.  when_disabled_wait is how often you should
         poll if the screensaver is disabled and you are using XSS for
         your idle threshold.
-        
+
         If you set idle_threshold to 'xss', it will say that your
         threshold for becoming idle is whenever the screensaver activates.
         If you don't use the XScreenSaver extension or otherwise want
@@ -135,10 +142,11 @@ class XSSTracker:
         # active.  all subsequent calls will only tell you if the screensaver
         # state has changed
         self.last_state = xss.ScreenSaverDisabled
+
     def check_idle(self):
         """Returns a tuple:
         (state_change, suggested_time_till_next_check, idle_time)
-        
+
         suggested_time_till_next_check and idle_time is in milliseconds.
         state_change is one of:
             None - No change in state
@@ -150,8 +158,8 @@ class XSSTracker:
         every time."""
         try:
             self.info = get_info()
-        except RuntimeError: # XSS can raise a RuntimeError if the
-                             # XSS extension cannot be found.
+        except RuntimeError:  # XSS can raise a RuntimeError if the
+            # XSS extension cannot be found.
             return ("disabled", self.when_disabled_wait, 0)
 
         state = self.info.state
@@ -161,7 +169,7 @@ class XSSTracker:
         if state == ScreenSaverDisabled:
             self.last_state = state
             return ("disabled", self.when_disabled_wait, 0)
-        
+
         # wait_time is how long they should wait before polling again.
         if state == ScreenSaverOff:
             # if the screen saver is off, til_or_since will tell us
@@ -178,17 +186,18 @@ class XSSTracker:
         # change is whether or not we have changed.  if we have, we use
         # a string to describe the change
         if state != self.last_state:
-            if state == ScreenSaverOff: # if we've changed from On to Off
-                                            # they're not idle anymore
+            if state == ScreenSaverOff:  # if we've changed from On to Off
+                # they're not idle anymore
                 change = 'unidle'
             else:
-                change = 'idle' # if we've changed from Off to On, they've gone
-                                # idle
-        else: # otherwise, they haven't changed state
+                change = 'idle'  # if we've changed from Off to On, they've gone
+                # idle
+        else:  # otherwise, they haven't changed state
             change = None
 
         self.last_state = state
         return (change, wait_time, self.info.idle)
+
 
 if __name__ == "__main__":
     # this demo shows how you might write a simple poller
@@ -198,6 +207,6 @@ if __name__ == "__main__":
         info = i.check_idle()
         print(time.asctime(), info, end=' ')
         # don't poll more often than 2 seconds
-        wait_time = max(info[1] / 1000, 2) 
+        wait_time = max(info[1] / 1000, 2)
         print("wait:", wait_time)
         time.sleep(wait_time)
